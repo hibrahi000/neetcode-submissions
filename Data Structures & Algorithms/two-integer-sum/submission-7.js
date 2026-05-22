eed58866eb625bd0ba50708e0c0baca6233333a6
Hashmat Ibrahimi <hashmatibrahimi0711@gmail.com>
2026-05-21 20:39:13 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     * 
+     * Pattern: 2 pointer 
+     * target : Logic mapping and search for compliment
+     * return : indicies 
+     * 
+     * 
+     */
+    twoSum(nums, target) {
+      
+      let m = new Map();
+
+      for(let i = 0; i<nums.length; i++){
+        
+        
+        let comp = target - nums[i] 
+        
+        if(m.has(comp)) return [m.get(comp), i];
+        
+        
+        m.set(nums[i], i);
+      }
+    }
+}
