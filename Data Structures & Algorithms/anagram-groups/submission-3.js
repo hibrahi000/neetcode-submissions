5f7f8a15d7d19a57ed13d4115c1329abb84d74f1
Hashmat Ibrahimi <hashmatibrahimi0711@gmail.com>
2026-05-23 17:00:17 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     * 
+     * we need to make anagrams and store them
+     * one loop to add anagram to a map but we can also sort it as a key 
+     */
+    groupAnagrams(strs) {
+
+    let m  = new Map();
+
+    strs.forEach(s => {
+        const sortedS = s.split("").sort().join();
+        if(!m.has(sortedS)) m.set(sortedS, [])
+        m.get(sortedS).push(s);
+    })
+    return [...m.values()];
+    }
+}
