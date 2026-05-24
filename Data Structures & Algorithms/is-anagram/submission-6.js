a98a3d19125739dd4f091ff00fa2a2aaa42c8ca0
Hashmat Ibrahimi <hashmatibrahimi0711@gmail.com>
2026-05-23 23:50:53 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) return false;
+        const balance = new Map();
+        for (const char of s) {
+            balance.set(char, (balance.get(char) || 0) + 1);
+        }
+        for (const char of t) {
+            if (!balance.get(char)) return false;
+            balance.set(char, (balance.get(char) || 0) - 1);
+        }
+        return true;
+    }
+}
