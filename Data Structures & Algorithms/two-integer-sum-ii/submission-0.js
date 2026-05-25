6e625fc5cf0381c4feb36a9057cdc9007b7fba1f
Hashmat Ibrahimi <hashmatibrahimi0711@gmail.com>
2026-05-25 18:09:58 -0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     *
+     * sorted in increasing order meaning binary search
+     * return index
+     *
+     */
+
+    twoSum(numbers, target) {
+        let l = 0;
+        let r = numbers.length - 1;
+
+        while (l < r) {
+            let currSum = numbers[l] + numbers[r];
+
+            if (currSum > target) {
+                r--;
+                
+            } else if (currSum < target) {
+                l++;
+                
+            } else {
+                return [l+1, r+1];
+            }
+        }
+        return [];
+    }
+}
