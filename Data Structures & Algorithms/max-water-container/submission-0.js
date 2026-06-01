b2ec75a166ca0435502f7f478b76877947b08cfb
Hashmat Ibrahimi <hashmatibrahimi0711@gmail.com>
2026-06-01 16:06:24 -0400

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     *
+     * itterate and check formula
+     * if min is left then increase pointer left to the next because we just want the larger size to be held
+     * if min is right then decrease right
+     * if calculated area is greater than max area then reset and continue while loop
+     */
+    maxArea(heights) {
+        let maxArea = 0;
+        let left = 0;
+        let right = heights.length - 1;
+
+        while (left < right) {
+            let height = Math.min(heights[left], heights[right]);
+            let width = right - left;
+            let area = height * width;
+
+            console.log(`${height} X ${width} = ${area} ---> maxArea = ${maxArea}`)
+
+            if (area > maxArea) maxArea = area;
+            height == heights[left] ? left++ : right--;
+        }
+        return maxArea;
+    }
+}
